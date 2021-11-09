1f853ea7d71cc5a01c3cd11a5bc70b53248a356a
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-11-09 07:36:29 +0100

    WIP:: Placeholed

--- resources/views/partials/forms/components/pgp.blade.php
+++ resources/views/partials/forms/components/pgp.blade.php
@@ -20,7 +20,19 @@
                     target="_blank">Click</a>
             </div>
             <textarea name="message_encrypted" class="form-control" id="message-encrypted" rows="5"
-                required></textarea>
+                placeholed="-----BEGIN PGP PUBLIC KEY BLOCK-----
+                Version: Keybase OpenPGP v1.0.0
+       
+                xo0EYYoNDwEEAKw9D69mGj4VSNnJOHrK4SjutgeAqWCFvfQB/EoPZKqnNZQlUQLG
+                gJ0TMH2Wes1ioOdUDNm0/HgruyJQxLn0iZX0yhMh/E7vCv62DKVVqHRSfIHKcyjG
+                hDCPoqIJ6odaWKLxzetkkr/kFEZTrWxqmVX9tsroYBSZKk5AsQwjeLkpABEBAAHN
+                J0V1Z2VueSBMeXViZXpueXkgPGtleWxvcnViaW9AZ21haWwuY29tPsKtBBMBCgAX
+                BQJhig0PAhsvAwsJBwMVCggCHgECF4AACgkQOAbBWAeMSGQxPwQAqbzKWatXyTC3
+                4YX7Qs+NUcmupn0IBPf7sn8TEsGXd9veBH+kPRYkzyPaIqwEs8zgh/r1nYNqYso6
+                zg==
+                =cCId
+                -----END PGP PUBLIC KEY BLOCK-----
+                " required></textarea>
         </div>
     </div>
     <div class="form-group">
@@ -30,5 +42,4 @@
         <input name="message_decrypted" type="text" class="form-control" id="exampleInputEmail1"
             aria-describedby="emailHelp" placeholder="Enter test message">
     </div>
-
 </div>
