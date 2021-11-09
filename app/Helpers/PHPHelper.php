afdd0291306ec90201849254a8eb8e02c845e962
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-11-09 07:19:44 +0100

    WIP :: Generate Session encrypt message register

--- app/Helpers/PHPHelper.php
+++ app/Helpers/PHPHelper.php
@@ -15,7 +15,10 @@ class PHPHelper
             $res = gnupg_init();
             $rtv = gnupg_import($res, $pubkey);
             $rtv = gnupg_addencryptkey($res, $rtv["fingerprint"]);
-            $enc = gnupg_encrypt($res, "just a test to see if anything works");
+
+            session(['publicUserKey' => 'Hello friend:' . niqid() . niqid()]);
+
+            $enc = gnupg_encrypt($res, session('publicUserKey'));
             return $enc;
         } catch (Exception $e) {
             return null;
