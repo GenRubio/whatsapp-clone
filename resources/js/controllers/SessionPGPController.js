d51c86fcb49c1c47bec4ae69fe7f5f4368b45ca6
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-11-10 07:28:12 +0100

    a

--- resources/js/controllers/SessionPGPController.js
+++ resources/js/controllers/SessionPGPController.js
@@ -24,6 +24,7 @@ const SessionPGPController = {
         });
     },
     savePrivateKeysHandler(e){
+        alert(1);
         e.preventDafault();
         const $this = this;
         const item = $(e.currentTarget);
