35ada4b160b12c88f0fbf0bc641af66f99112326
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-11-13 12:33:53 +0100

    a

--- resources/views/components/messages/user-message.blade.php
+++ resources/views/components/messages/user-message.blade.php
@@ -1,6 +1,6 @@
 <div class="my-message d-flex justify-content-end w-100">
     <div class="my-message-container">
-        {{ $message }}
+        {!! $message !!}
         <span>
             {{ $hour }}
         </span>
