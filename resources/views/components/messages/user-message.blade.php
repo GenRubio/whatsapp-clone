3ca76e89add1db40cbfa17539c82015e5b1765a7
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-11-13 13:16:58 +0100

    a

--- resources/views/components/messages/user-message.blade.php
+++ resources/views/components/messages/user-message.blade.php
@@ -1,7 +1,7 @@
 <div class="my-message d-flex justify-content-end w-100">
     <div class="my-message-container">
         <div>
-            {!! decryptMessage(nl2br(e($message->message_sender), false)) !!}
+            {!! decryptMessage($message->message_sender) !!}
         </div>
         <span>
             {{ getHourMessage($message->date) }}
