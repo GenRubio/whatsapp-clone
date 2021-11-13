1228af021923ad9bf6e3557ee3cf173ed113a488
Gen Rubio <62005965+GenRubio@users.noreply.github.com>
2021-11-13 13:45:26 +0100

    d

--- resources/views/components/messages/friend-message.blade.php
+++ resources/views/components/messages/friend-message.blade.php
@@ -1,6 +1,6 @@
 <div class="friend-message d-flex justify-content-start w-100">
     <div class="friend-message-container">
-        {{ decryptMessage($message->message) }}
+        {{ $message->message }}
         <span>
             {{ getHourMessage($message->date) }}
         </span>
